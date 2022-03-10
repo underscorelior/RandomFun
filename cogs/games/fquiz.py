4117d92caad4b37463a0d258db4db1cc20f0c667
underscorelior <newlioryt@gmail.com>
2022-03-10 00:04:13 -0800

    Why do replies break buttons??1//!/!

--- cogs/games/fquiz.py
+++ cogs/games/fquiz.py
@@ -24,12 +24,12 @@ class FlagQuiz(commands.Cog):
 		ca=await checkansf(data, ansloc,quizans)
 		btnans=[[Button(label=ca[0], emoji="🇦", style=ButtonStyle.blue, custom_id=1),Button(label=ca[1],emoji='🇧', style=ButtonStyle.blue, custom_id=2),Button(label=ca[2], emoji="🇨", style=ButtonStyle.blue, custom_id=3),Button(label=ca[3], emoji='🇩', style=ButtonStyle.blue, custom_id=4)]]
 		msem = discord.Embed(title=f'Which country does this flag belong to?',color=0x1860cc, timestamp = datetime.utcnow()).set_image(url=quizans["flags"]["png"]).set_footer(text=ctx.author,icon_url=ctx.author.avatar_url)
-		message = await ctx.reply(embed=msem,components=btnans)
+		message = await ctx.send(content=ctx.author.mention,embed=msem,components=btnans)
 		try: 
 			ansch = await self.bot.wait_for('button_click',check=lambda inter: inter.message.id == message.id and inter.user.id == ctx.author.id,timeout=15)
 		except asyncio.TimeoutError: 
 			return await message.edit(embed=await toembedt(f'Which country does this flag belong to? \nAnswer: `{quizans["name"]["common"]}`',quizans["flags"]["png"]),components=[[Button(emoji="🇦",style=ButtonStyle.grey,disabled=True),Button(emoji='🇧',style=ButtonStyle.grey,disabled=True),Button(emoji="🇨",style=ButtonStyle.grey,disabled=True),Button(emoji='🇩',style=ButtonStyle.grey,disabled=True)]])
-		
+
 		if int(ansch.custom_id) == ansloc:
 			await message.edit(embed=discord.Embed(title='Win',description=f'Which country does this flag belong to? \nAnswer: `{quizans["name"]["common"]}`', color=0x3cb556, timestamp = datetime.utcnow()).set_thumbnail(url=quizans["flags"]["png"]).set_footer(text=ctx.author,icon_url=ctx.author.avatar_url),components=await winbtn(ansloc))
 		else:
